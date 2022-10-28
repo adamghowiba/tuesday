023bb0b188df407fbbf6f9a643071356210699bb
Adam <adamware99@hotmail.com>
2022-10-28 14:07:21 +0400

    Intital Commit - Let's see if this gets me offers.

--- apps/work/pages/_app.tsx
+++ apps/work/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import '../lib/styles/global.scss';
+
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
